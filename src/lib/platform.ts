1a09794a9334eb1af497d9592407fd3f9d89f33d
Jen Garcia <jen@cuibonobo.com>
2018-11-08 08:03:30 -0500

    Promisify reading and writing from a file

--- src/lib/platform.ts
+++ src/lib/platform.ts
@@ -1,3 +1,4 @@
+import * as fs from "fs";
 
 /*
 From https://stackoverflow.com/questions/19275776/node-js-how-to-get-the-os-platforms-user-data-folder#26227660
@@ -9,3 +10,31 @@ The expected result is:
   * Linux: '~/.config'
 */
 export const userDataDir = process.env.APPDATA || (process.platform === 'darwin' ? process.env.HOME + 'Library/Preferences' : `/home/${process.env.USER}/.config`);
+
+export const writeFile = (path: string, data: string): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, data, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+export const readFile = (path: string): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    fs.exists(path, (exists:boolean) => {
+      if (!exists) {
+        return reject();
+      }
+      fs.readFile(path, (err, data:Buffer) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(data.toString());
+      });
+    });
+  });
+};
--- src/models/Settings.ts
+++ src/models/Settings.ts
@@ -1,7 +1,6 @@
-import * as fs from "fs";
 import * as path from "path";
 import { ISettingsJSON } from "../lib/interfaces";
-import { userDataDir } from "../lib/platform";
+import { readFile, userDataDir, writeFile } from "../lib/platform";
 
 export default class Settings {
   public static readonly filename: string = "journal.json";
@@ -9,32 +8,11 @@ export default class Settings {
 
   public static async generateSettings(repositoryDir: string) {
     const obj:ISettingsJSON = {repositoryDir};
-    return new Promise((resolve, reject) => {
-      fs.writeFile(Settings.filePath, JSON.stringify(obj), (err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve();
-        }
-      });
-    });
+    await writeFile(Settings.filePath, JSON.stringify(obj));
   }
   private settings?:ISettingsJSON;
 
   public async readFromFile() {
-    return new Promise((resolve, reject) => {
-      fs.exists(Settings.filePath, (exists:boolean) => {
-        if (!exists) {
-          return reject();
-        }
-        fs.readFile(Settings.filePath, (err, data:Buffer) => {
-          if (err) {
-            return reject(err);
-          }
-          this.settings = JSON.parse(data.toString());
-          resolve();
-        });
-      });
-    });
+    this.settings = JSON.parse(await readFile(Settings.filePath));
   }
 }
